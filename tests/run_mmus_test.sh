4fa1e2e11bc484052db7032d3c328a9461eece89
rachadele <raschwaa@gmail.com>
2025-07-10 13:31:29 -0700

    update tests

--- tests/run_mmus_test.sh
+++ tests/run_mmus_test.sh
@@ -5,6 +5,6 @@ nextflow sc-annotate.nf \
 	-params-file params.mm.json \
 	--study_names /space/grp/rschwartz/rschwartz/cell_annotation_cortex.nf/study_names_mouse.txt \
 	-process.executor slurm \
-	--process_samples true \
+	--process_samples false \
 	-resume
 	
