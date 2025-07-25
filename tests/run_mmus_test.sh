e1b34b1b8d0f3ba3f818414e02b57453f5bd1708
rachadele <raschwaa@gmail.com>
2025-07-25 11:47:10 -0700

    add option to replace existing CTA

---
+++ tests/run_mmus_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nextflow sc-annotate.nf \
+	-profile conda \
+	-params-file params.mm.json \
+	--study_names GSE152715.1 \
+	-process.executor local \
+	--process_samples false \
+	-resume \
+	--mask false 
+	
