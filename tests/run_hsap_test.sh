9ea5b3efdd962d1ecbb77883649c4172b7abad89
rachadele <raschwaa@gmail.com>
2025-07-28 13:08:23 -0700

    update multiQC example

--- tests/run_hsap_test.sh
+++ tests/run_hsap_test.sh
@@ -3,7 +3,7 @@
 nextflow sc-annotate.nf \
 	-profile conda \
 	-params-file params.hs.json \
-	--studies_path /space/grp/rschwartz/rschwartz/cell_annotation_cortex.nf/results/homo_sapiens_subsample_ref_500_2025-07-21_13-55-05/mex/Velmeshev_et_al.2 \
+	--study_names tests/study_names_human.txt \
 	-process.executor local \
 	-resume
 	
