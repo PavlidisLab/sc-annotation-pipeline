0f2b36bfcf35a7bbb905ee694df4d6405995ddc9
rachadele <raschwaa@gmail.com>
2025-07-30 11:55:40 -0700

    support multiple names and paths in command line args

--- tests/run_hsap_test.sh
+++ tests/run_hsap_test.sh
@@ -3,7 +3,7 @@
 nextflow sc-annotate.nf \
 	-profile conda \
 	-params-file params.hs.json \
-	--study_paths /space/grp/rschwartz/rschwartz/get_gemma_data.nf/psychEncode.txt_author_true_sample_split_true/mex_dirs/Velmeshev_et_al.1 \
+	--study_paths "/space/grp/rschwartz/rschwartz/get_gemma_data.nf/psychEncode.txt_author_true_sample_split_true/mex_dirs/Velmeshev_et_al.1 /space/grp/rschwartz/rschwartz/get_gemma_data.nf/psychEncode.txt_author_true_sample_split_true/mex_dirs/Velmeshev_et_al.2" \
 	-process.executor local \
 	--process_samples false \
 	--mask false \
--- tests/run_mmus_test.sh
+++ tests/run_mmus_test.sh
@@ -3,7 +3,7 @@
 nextflow sc-annotate.nf \
 	-profile conda \
 	-params-file params.mm.json \
-	--study_paths /space/grp/rschwartz/rschwartz/get_gemma_data.nf/study_names_mouse.txt_author_true_process_samples_true/mex/GSE247339.2 \
+	--study_paths "/space/grp/rschwartz/rschwartz/get_gemma_data.nf/study_names_mouse.txt_author_true_process_samples_true/mex/GSE247339.2" \
 	-process.executor local \
 	--process_samples false \
 	-resume \
