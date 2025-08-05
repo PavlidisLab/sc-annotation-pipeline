ec62fe32b69275b2bddedac8e087e7ec255cb10e
rachadele <raschwaa@gmail.com>
2025-08-05 13:29:35 -0700

    add new tests

---
+++ tests/run_hsap_names_file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nextflow sc-annotate.nf \
+	-profile conda \
+	-params-file params.hs.json \
+	--study_names /space/grp/rschwartz/rschwartz/cell_annotation_cortex.nf/tests/study_names_human.txt \
+	-process.executor local \
+	--process_samples false \
+	--mask false \
+	-resume
+	
